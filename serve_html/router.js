ba1727ac9047d4f8294fce96185b8e35538a06c7
mordini <yahewisthedevil@yahoo.com>
2021-01-14 11:23:37 -0500

    6.5 added router.js functions to exports object, handler, get and post functions added

--- serve_html/router.js
+++ serve_html/router.js
@@ -1,10 +1,41 @@
 'use strict';
 
-const sendErrorResponse = res => { // Create error-handling function
+const httpStatus = require('http-status-codes');
+const htmlContentType = {
+    'Content-Type': 'text/html'
+}
+const routes = { //define a routes object to store routes mapped to POST and GET requests
+    'GET': {
+        '/info': (req, res) => {
+            res.writeHead(httpStatus.StatusCodes.OK, {
+                'Content-Type': 'text/plain'
+            })
+            res.end('Welcome to the Info Page!')
+        }
+    },
+    'POST': {}
+};
 
-    res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
-        'Content-Type': 'text/html'
-    });
-    res.write(`<h1>${res.statusCode}:${res.statusMessage}</br></br>What did you DO!?</h1>`);
-    res.end();
+// use the exports object to give other files access to the functionality
+exports.handle = (req, res) => { //Create a function called 'handle' to process route callback functions
+
+    try {
+        if (routes[req.method][req.url]) {
+            routes[req.method][req.url](req, res);
+        } else {
+            res.writeHead(httpStatus.StatusCodes.NOT_FOUND, htmlContentType);
+            res.end('<h1> NO such file exists</h1>');
+        }
+    } catch (ex) {
+        console.log(`error: ${ex}`);
+    }
+};
+
+// Build 'get' and 'post' functions to register routes from main.js
+// more methods will go in here later
+exports.get = (url, action) => {
+    routes['GET'][url] = action;
+};
+exports.post = (url, action) => {
+    routes['POST'][url] = action;
 };
