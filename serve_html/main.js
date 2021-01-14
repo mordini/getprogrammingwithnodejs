db70060e49405f27270546d3dd82a6b7b361a3b1
mordini <yahewisthedevil@yahoo.com>
2021-01-14 11:48:34 -0500

    6.6 imported router.js into main.js, added handlers in main.js

--- serve_html/main.js
+++ serve_html/main.js
@@ -5,17 +5,36 @@ const http = require('http');
 const httpStatus = require('http-status-codes');
 const fs = require('fs'); // file system module
 
-// // set up route mapping for html files
-// // NO LONGER USED DUE TO getViewUrl function interpolation of view request
-// const routeMap = {
-//     '/': 'views/index.html'
-// };
+const router = require('./router');
+const plainTextContentType = {
+    'Content-Type': 'text/plain'
+}
+const htmlContentType = {
+    'Content-Type': 'text/html'
+}
 
-// interpolate the URL into the filepath (EX. turn '/about' URL into 'views/about.html')
-const getViewUrl = (url) => {
-    console.log(`request url: ${url}`);
-    return `views${url}.html`; // the '/' exists as first character in the url request already
+const customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+        if (errors) {
+            console.log(`Error reading the file: ${errors}`);
+        }
+        res.end(data);
+    });
 };
+
+// register routes with 'get' and 'post'
+router.get('/', (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, plainTextContentType);
+    res.end('THIS IS A PLAINTEXT INDEX');
+});
+router.get('/index.html', (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, htmlContentType);
+    customReadFile('views/index.html', res);
+});
+router.post('/', (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, plainTextContentType);
+    res.end('THIS HAS BEEN POSTED');
+});
 
 const sendErrorResponse = res => { // Create error-handling function
 
@@ -26,55 +45,77 @@ const sendErrorResponse = res => { // Create error-handling function
     res.end();
 };
 
-http
-    .createServer((req, res) => {
-        let url = req.url;  //store the request URL into a variable
-        if (url.indexOf('.html') !== -1) { // check for file extension
-            res.writeHead(httpStatus.StatusCodes.OK, {
-                'Content-Type': 'text/html'
-            });
-            customReadFile(`./views${url}`, res);
-        } else if (url.indexOf('.js') !== -1) {
-            res.writeHead(httpStatus.StatusCodes.OK, {
-                'Content-Type': 'text/javascript'
-            });
-            customReadFile(`./public/js${url}`, res);
-
-        } else if (url.indexOf('.css') !== -1) {
-            res.writeHead(httpStatus.StatusCodes.OK, {
-                'Content-Type': 'text/css'
-            });
-            customReadFile(`./public/css${url}`, res);
-
-        } else if (url.indexOf('.png') !== -1) {
-            res.writeHead(httpStatus.StatusCodes.OK, {
-                'Content-Type': 'image/png'
-            });
-            customReadFile(`./public/images${url}`, res);
-
-        } else {
-            sendErrorResponse(res);
-        }
-    })
-    .listen(port);
+http.createServer(router.handle).listen(port)
 
 console.log(`the server has started on port: ${port}`);
 
-const customReadFile = (file_path, res) => { // look for a file by the name requested
-    if (fs.existsSync(file_path)) { // check if the file exists
-        fs.readFile(file_path, (error, data) => { // error comes first
-            if (error) {
-                console.log(error);
-                sendErrorResponse(res);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
-    } else {
-        sendErrorResponse(res);
-    }
-};
+
+
+// OLD CODE BELOW, REMOVE LATER
+
+// // set up route mapping for html files
+// // NO LONGER USED due to getViewUrl function interpolation of view request
+// const routeMap = {
+//     '/': 'views/index.html'
+// };
+
+// // interpolate the URL into the filepath (EX. turn '/about' URL into 'views/about.html')
+// const getViewUrl = (url) => {
+//     console.log(`request url: ${url}`);
+//     return `views${url}.html`; // the '/' exists as first character in the url request already
+// };
+
+
+// http
+//     .createServer((req, res) => {
+//         let url = req.url;  //store the request URL into a variable
+//         if (url.indexOf('.html') !== -1) { // check for file extension
+//             res.writeHead(httpStatus.StatusCodes.OK, {
+//                 'Content-Type': 'text/html'
+//             });
+//             customReadFile(`./views${url}`, res);
+//         } else if (url.indexOf('.js') !== -1) {
+//             res.writeHead(httpStatus.StatusCodes.OK, {
+//                 'Content-Type': 'text/javascript'
+//             });
+//             customReadFile(`./public/js${url}`, res);
+
+//         } else if (url.indexOf('.css') !== -1) {
+//             res.writeHead(httpStatus.StatusCodes.OK, {
+//                 'Content-Type': 'text/css'
+//             });
+//             customReadFile(`./public/css${url}`, res);
+
+//         } else if (url.indexOf('.png') !== -1) {
+//             res.writeHead(httpStatus.StatusCodes.OK, {
+//                 'Content-Type': 'image/png'
+//             });
+//             customReadFile(`./public/images${url}`, res);
+
+//         } else {
+//             sendErrorResponse(res);
+//         }
+//     })
+//     .listen(port);
+
+// console.log(`the server has started on port: ${port}`);
+
+// REPLACED IN 6.6, handling and managing routes in main.js
+// const customReadFile = (file_path, res) => { // look for a file by the name requested
+//     if (fs.existsSync(file_path)) { // check if the file exists
+//         fs.readFile(file_path, (error, data) => { // error comes first
+//             if (error) {
+//                 console.log(error);
+//                 sendErrorResponse(res);
+//                 return;
+//             }
+//             res.write(data);
+//             res.end();
+//         });
+//     } else {
+//         sendErrorResponse(res);
+//     }
+// };
 
 /*
 
