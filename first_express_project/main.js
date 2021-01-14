c43e6c20bf8264fb8c8efe75a969127eb55eaccd
mordini <yahewisthedevil@yahoo.com>
2021-01-14 16:34:48 -0500

    added basic express.js functionality

--- first_express_project/main.js
+++ first_express_project/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const port = 3000;
-const express = require(express); // Require express
+const express = require('express'); // Require express
 const app = express(); // Assign express to the app
 
 // set up a 'GET' route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, Universe!'); // issue a response from the server to the client with res.send
-}).listen(port, () => { // listen on the specified port
+
+    if (1 === 1) { // just checking things out
+        console.log(`req.params:`);
+        console.log(req.params);
+        console.log(`req.body: ${req.body}`);
+        console.log(`req.url: ${req.url}`);
+        console.log(`req.query:`);
+        console.log(req.query);
+    }
+
+}).listen(port, (req) => { // listen on the specified port
     console.log(`The Express.js server has started and is listening on port: ${port}`);
 });
