6ef874849e5a08f094a0ed0fcb165d57d90f8fea
mordini <yahewisthedevil@yahoo.com>
2021-01-15 15:58:51 -0500

    removed old code, added example comments

--- express_routes/controllers/homeController.js
+++ express_routes/controllers/homeController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // create a new post route for the home page
-exports.post = (req, res) => {
+exports.post = (req, res) => {  // try:  curl --date "first_name=Steve&last_name=Reeves" http://localhost:3000
     console.log(`log post: req.body`);
     console.log(req.body);  // log the request's body
     console.log(`log post: req.query`);
@@ -11,18 +11,18 @@ exports.post = (req, res) => {
 
 // adding route to get URL parameters
 exports.sendReqParam = (req, res) => {
-    let veg = req.params.vegetable;
+    let veg = req.params.vegetable;  //catch the vegetable param in /items (try /items/corn)
     res.send(`this is the page for ${veg}, you rebel scum.`);
 };
 
 exports.logRequestUrl = (req, res, next) => {
-    console.log(`homeController middleware: logRequestUrl()`);
+    console.log(`homeController export middleware: logRequestUrl`);
     console.log(`request made to ${req.url}`); // log the request for demo
     next(); //call the next function.  without this, it will hang
 };
 
 exports.logRequestQuery = (req, res, next) => {
-    console.log(`homeController middleware: logRequestQuery`);
+    console.log(`homeController export middleware: logRequestQuery`);
     console.log(req.query); // checking the query (parameters) in the URL (example): http://localhost:3000/?firstname=steve&lastname=reeves
     next(); //call the next function.  without this, it will hang
 };
--- express_routes/main.js
+++ express_routes/main.js
@@ -5,33 +5,9 @@ const express = require('express');
 const app = express();
 const homeController = require('./controllers/homeController');
 
-// MOVED TO controller
-// JUST AN EXAMPLE OF POST
-// app.post('contact', (req, res) => { // handle requestsd with the express.js POST method
-//     res.send('contact information submitted successfully');
-// });
-
-// BEGIN MIDDLEWARE TEST SECTION, RUN IN ORDER
-
-app.use((req, res, next) => { // define a middleware function using 'use', use it in app
-    console.log(`main.js app.use middleware ONE ftw`);
-    // console.log(`request made to ${req.url}`); // log the request for demo
-    next(); //call the next function.  without this, it will hang
-});
-
 // Apply middleware from homeController.js
 app.use(homeController.logRequestQuery);
 app.use(homeController.logRequestUrl);
-
-// middleware function two, for demonstration
-app.use((req, res, next) => {
-    console.log(`main.js app.use middleware TWO ftw`);
-    // console.log(`requested query:`);
-    // console.log(req.query); // checking the query (parameters) in the URL (example): http://localhost:3000/?firstname=steve&lastname=reeves
-    next();
-});
-
-// END MIDDLEWARE TEST SECTION, RUN IN ORDER
 
 // tell express.js to parse URL-encoded data
 app.use(
@@ -44,26 +20,10 @@ app.use(
 app.use(express.json());
 
 // handle post route for the home page using homeController export function 'post'
-app.post('/', homeController.post);  // curl --date "first_name=Steve&last_name=Reeves" http://localhost:3000
+app.post('/', homeController.post);
 
 // Handle get requests to '/items/:vegetable' using homeController export function 'sendReqParam'
 app.get('/items/:vegetable', homeController.sendReqParam);
-
-// MOVED TO controller
-// // create a new post route for the home page
-// app.post('/', (req, res) => {
-//     console.log(req.body);  // log the request's body
-//     console.log(req.query);
-//     res.send('POST successful!');
-// });
-
-// // adding route to get URL parameters
-// app.get('/items/:vegetable', (req, res) => { //catch the vegetable param in /items (try /items/corn)
-//     console.log(`end of the line`);
-
-//     let veg = req.params.vegetable;
-//     res.send(`this is the page for ${veg}`);
-// });
 
 // turn the server on
 app.listen(port, () => {
