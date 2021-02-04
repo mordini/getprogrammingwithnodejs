ec51a3f433fb5dc93ab6a4a007e22473a056e1b5
mordini <yahewisthedevil@yahoo.com>
2021-02-04 13:10:10 -0500

    moved schema to models

--- unit_3/lesson_14/recipe_app/main.js
+++ unit_3/lesson_14/recipe_app/main.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const theDB = 'mongodb://localhost:27017/recipe_db';
+const Subscriber = require('./models/subscriber');
 
 mongoose.connect(theDB, // set up the connection to the database
     {
@@ -16,38 +17,48 @@ db.once('open', () => { // log a message when the application connects to the da
     console.log('Conected to MongoDB using Mongoose!');
 });
 
-const subscriberSchema = mongoose.Schema({ // create a new schema with mongoose
-    name: String,
-    email: String,
-    zipCode: Number  // adding schema properties
+if (1 === 0) { // just to keep things neat in the DB
+
+    // TWO WAYS TO GENERATE NEW OBJECTS
+
+    // 01. create and save from instances of the model
+
+    // instantiate a new Subscriber object
+    var subscriber1 = new Subscriber({
+        name: 'Steve Reeves from new',
+        email: 'fake@notreal.com',
+    });
+
+    // then save the new Subscriber object data
+    subscriber1.save((error, savedDocument) => { // save to the database
+        if (error) console.log(error); // pass potential errors to the next middleware function
+        console.log(savedDocument); // log saved data
+    });
+
+    // 02 using create function, which does 'new' and 'save' in one step
+    Subscriber.create({
+        name: 'Steve Reeves from create',
+        email: 'fake@notreal.com'
+    },
+        //function (error, savedDocument) {  // example uses function but...
+        (error, savedDocument) => {  // i just like using arrow functions
+            if (error) console.log(error);
+            console.log(savedDocument);
+        }
+    );
+};
+
+// let's find one of them, just for fun
+var myQuery = Subscriber
+    .findOne({
+        name: 'Steve Reeves from new'
+    })
+    .where('email', /fake/);
+
+myQuery.exec((error, data) => { // run the query with a callback to handle data and errors
+    if (data) {
+        console.log(data)
+    } else {
+        console.log('not found');
+    };
 });
-
-const Subscriber = mongoose.model('Subscriber', subscriberSchema) // apply defined schema to a model
-
-// TWO WAYS TO GENERATE NEW OBJECTS
-
-// 01. create and save from instances of the model
-
-// instantiate a new Subscriber object
-var subscriber1 = new Subscriber({
-    name: 'Steve Reeves from new',
-    email: 'fake@notreal.com',
-});
-
-// then save the new Subscriber object data
-subscriber1.save((error, savedDocument) => { // save to the database
-    if (error) console.log(error); // pass potential errors to the next middleware function
-    console.log(savedDocument); // log saved data
-});
-
-// 02 using create function, which does 'new' and 'save' in one step
-Subscriber.create({
-    name: 'Steve Reeves from create',
-    email: 'fake@notreal.com'
-},
-    //function (error, savedDocument) {  // example uses function but...
-    (error, savedDocument) => {  // i just like using arrow functions
-        if (error) console.log(error);
-        console.log(savedDocument);
-    }
-);
--- unit_3/lesson_14/recipe_app/models/subscriber.js
+++ unit_3/lesson_14/recipe_app/models/subscriber.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const mongoose = require('mongoose'),  // need to require mongoose because both schema and model use Mongoose methods to work, and Node.js loads a module into the project only once, does not load twice
+    subscriberSchema = mongoose.Schema({ // create a new schema with mongoose
+        name: String,
+        email: String,
+        zipCode: Number  // adding schema properties
+    });
+
+// exporting  the Subscriber model as the only module export
+module.exports = mongoose.model('Subscriber', subscriberSchema) // apply defined schema to a model
